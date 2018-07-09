3623e1ffb41b4cf936b6ec8669abd130ce7146f5
Unknown <egor.v.kalmykov@gmail.com>
2018-07-09 13:39:17 +0300

    Я сделаль

--- main.js
+++ main.js
@@ -27,12 +27,49 @@ while(word[j] != 'p' && j <word.length)
 }
 
 if(j == word.length) {
-    console.log('Хер вам');
+    //console.log('Хер вам');
 }
 else {
-    console.log(j);
+    //console.log(j);
 }
 
-console.log(max);
+//console.log(max);
 
+var names = ['Ilya', 'Egor', 'Sonya', 'Adruha', 'Alina', 'Mike', 'Jeremy'];
 
+for(var i = 0; i < names.length; i++){
+    var amount = names[i].length;
+    //console.log("hello, " + names[i] + "y tebya " + amount + " bykvy"); 
+}
+
+var word1 = 'qakfoisolfspouiughw2';
+var word2 = 'qtkfosohlfspouiughw2';
+
+var t = 0;
+for(var i = 0; i < word1.length; i++)
+{
+
+    if(word1[i] != word2[i]) t++;
+    
+}
+//console.log(t);
+
+var per = 12;
+var ans = 1;
+
+var fuct = 3;
+for(var i = fuct; i > 0; i--)
+{
+    ans *= i;
+}
+
+function FindFuct(number) {
+    if(number == 1)
+        return 1;
+    else
+        return number * FindFuct(number-1);
+}
+
+ans = FindFuct(12);
+
+console.log(ans);
